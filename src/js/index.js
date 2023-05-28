7e868766c3cbe9a268914a74e53d92302ce0243e
Gregory <49094805+grehhgory@users.noreply.github.com>
2023-05-28 19:30:54 +0800

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,3 @@
-// Direct zoom preventing function
-$(document).keydown(function (event) {
-	if (event.ctrlKey == true && (event.which == "61" || event.which == "107" || event.which == "173" || event.which == "109" || event.which == "187" || event.which == "189")) {
-		event.preventDefault();
-	}
-});
-
 // When main menu play button is clicked,
 $("#main-menu-play-btn").click(function () {
 	// hide:
